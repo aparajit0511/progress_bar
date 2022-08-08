cb8eac92333a09e1d8315726eabe278919debf2f
aparajit0511 <aparajit35@gmail.com>
2022-08-08 21:36:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ProgressBar from "./ProgressBar";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <ProgressBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ProgressBar.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from "react";
+
+export default function ProgressBar() {
+  //   const result = document.getElementsByClassName("bar");
+  //   console.log(result);
+
+  const [width, setwidth] = useState(0);
+  const [barStatus, setbarStatus] = useState(false);
+  const [IntervalId, setIntervalId] = useState(0);
+
+  //   const componentRef = useRef(null);
+  //   console.log(componentRef);
+
+  useEffect(() => {
+    var bar;
+    if (barStatus) {
+      bar = setInterval(() => {
+        setwidth((width) => width + 1);
+      }, 10);
+      setIntervalId(bar);
+    } else {
+      clearInterval(IntervalId);
+    }
+    console.log("bar", IntervalId);
+  }, [barStatus]);
+
+  const onClickHandler = () => {
+    setbarStatus(true);
+  };
+
+  const onStopHandler = () => {
+    setbarStatus(false);
+  };
+
+  return (
+    <div>
+      <div>ProgressBar</div>
+
+      <div
+        // ref={componentRef}
+        style={{
+          width: `${width}` + "px",
+          height: "20px",
+          backgroundColor: "grey",
+        }}
+      ></div>
+      <div>
+        <button onClick={onClickHandler}>Start</button>
+        <button onClick={onStopHandler}>Stop</button>
+      </div>
+    </div>
+  );
+}
